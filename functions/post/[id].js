b0114f7ef49b78d089c4d802c609ec4c385b7b34
alyamahalinipoltekpurbaya-ux <alyamahalini.poltekpurbaya@gmail.com>
2025-11-19 15:37:27 +0700

    Update [id].js

--- functions/post/[id].js
+++ functions/post/[id].js
@@ -97,7 +97,7 @@ function renderPage(post, SITE_URL) {
             <div class="download-container">
               <a target="_blank" rel="noopener noreferrer" class="download-btn" style="cursor: pointer;" onclick="openMyLinks()">
                 <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-zap"><polygon points="13 2 3 14 12 14 11 22 21 10 12 10 13 2"></polygon></svg>
-                <span>Get This Component</span>
+                <span>CLICK TO DOWNLOAD</span>
               </a>
             </div>
 
